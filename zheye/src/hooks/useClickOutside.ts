f6a3fa30f7814644b73cfe372c8058dd080c8543
10swan <1259496020@qq.com>
2022-01-25 23:55:11 +0800

    zhihu

--- zheye/src/hooks/useClickOutside.ts
+++ zheye/src/hooks/useClickOutside.ts
@@ -1,5 +1,6 @@
 import { ref, onMounted, onUnmounted, Ref } from 'vue'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
   const isClickOutside = ref(false)
   const handler = (e: MouseEvent) => {
