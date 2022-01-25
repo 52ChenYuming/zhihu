3fdc229a4e1043e6ed60de081e7499f5d9e0da08
10swan <1259496020@qq.com>
2022-01-25 23:53:38 +0800

    zhihu

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="container"></div>
-  <column-list :list="list"></column-list>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
 const testData:ColumnProps[] = [
   {
     id: 1,
@@ -33,13 +37,18 @@ const testData:ColumnProps[] = [
     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
+const currentUser: UserProps = {
+  isLogin: true,
+  name: '陈煜明'
+}
 
 export default defineComponent({
   name: 'App',
-  components: { ColumnList },
+  components: { ColumnList, GlobalHeader },
   setup () {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
--- zheye/src/components/ColumnList.vue
+++ zheye/src/components/ColumnList.vue
@@ -45,3 +45,7 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+
+</style>
