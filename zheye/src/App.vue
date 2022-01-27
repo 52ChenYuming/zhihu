0b9d25af7146ac088f7874187748e5ed5446a334
10swan <1259496020@qq.com>
2022-01-27 14:07:13 +0800

    Form

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <form action="">
+    <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
         <validate-input
           :rules="emailRules"
           v-model="emailVal"
-          placeholder="hello!"
-          type="password"
+          placeholder="请输入您的电子邮箱"
+          type="type"
         />
-        {{emailVal}}
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input
-          type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          v-model="emailRef.val"
-          @blur="validEmail"
-        >
-        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+        <label class="form-label">密码</label>
+        <validate-input
+          type="password"
+          placeholder="请输入密码"
+        />
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-    </form>
+      <template #submit>
+        <span class="btn btn-danger">提交</span>
+      </template>
+    </validate-form>
   </div>
 </template>
 
@@ -35,6 +31,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+import ValidateForm from './components/ValidateForm.vue'
 
 const testData:ColumnProps[] = [
   {
@@ -69,9 +66,9 @@ const currentUser: UserProps = {
 const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
-  components: { GlobalHeader, ValidateInput },
+  components: { GlobalHeader, ValidateInput, ValidateForm },
   setup () {
-    const emailVal = ref('cym@test.dev')
+    const emailVal = ref('')
     const emailRules: RulesProp = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' }
@@ -90,13 +87,17 @@ export default defineComponent({
         emailRef.message = 'should be valid message!'
       }
     }
+    const onFormSubmit = (result: boolean) => {
+      console.log('1234', result)
+    }
     return {
       list: testData,
       currentUser,
       emailRef,
       validEmail,
       emailRules,
-      emailVal
+      emailVal,
+      onFormSubmit
     }
   }
 })
