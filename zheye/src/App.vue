6b832ffe4136e5a993969d68bfb977335d804be8
10swan <1259496020@qq.com>
2022-01-29 01:03:34 +0800

    router

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,122 +1,44 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules"
-          v-model="emailVal"
-          placeholder="请输入您的电子邮箱"
-          type="type"
-          ref="inputRef"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-input
-          type="password"
-          placeholder="请输入密码"
-          :rules="passwordRules"
-          v-model="passwordVal"
-        />
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">提交</span>
-      </template>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
-
-const testData:ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 const currentUser: UserProps = {
-  isLogin: true,
-  name: '陈煜明'
+  isLogin: false,
+  name: 'viking'
 }
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
-  components: { GlobalHeader, ValidateInput, ValidateForm },
+  components: {
+    GlobalHeader
+  },
   setup () {
-    const inputRef = ref<unknown>()
-    const emailVal = ref('')
-    const passwordVal = ref('')
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' },
-      { type: 'range', min: { message: '你的密码至少包括六位，不能含有空格', length: 6 } },
-      { type: 'range', max: { message: '你的密码不能超过十二位，不能含有空格', length: 12 } }
-    ]
-
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    const validEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid message!'
-      }
-    }
-    const onFormSubmit = (result: boolean) => {
-      console.log('result', result)
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRef,
-      validEmail,
-      emailRules,
-      emailVal,
-      onFormSubmit,
-      passwordRules,
-      passwordVal,
-      inputRef
+      currentUser
     }
   }
 })
-
 </script>
 
 <style>
+
 </style>
